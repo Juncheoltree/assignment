4176adbc6ab5131e157a4c8ba86483724af04991
Juncheoltree <soultreejun@gmail.com>
2024-04-29 20:49:36 +0900

    프로젝트..올려버리기!

---
+++ OttukiDinning-develop/OttukiDinning/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  OttukiDinning
+//
+//  Created by t2023-m0056 on 4/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // 주석 추가
+        // 준철 깃 연습(?) + 브런치 만들기
+    }
+
+
+}
+
